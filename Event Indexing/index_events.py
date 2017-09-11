5b2ecd9c48359d88b1e8345c2a5adf06287610ae
ILIJAXPS\ivukotic <ivukotic@cern.ch>
2017-09-11 17:08:55 -0500

    init import

---
+++ Event Indexing/index_events.py
@@ -0,0 +1,31 @@
+import json
+import elasticsearch
+
+allDocs=[]
+
+with open("events.json") as json_file:
+    events = json.load(json_file)
+    print('events:',len(events))
+    for e in events:
+        ind='ar_events'
+        eid=str(e['eventnr'])
+        ev={'_index':ind, '_type':'event', 'description':e['description'], 'runnr':e['runnr'], '_id':eid}
+        allDocs.append(ev)
+        del e['description']
+        del e['runnr']
+        del e['eventnr']
+        for c,v in e.items():  #loop over collections
+            print(c)
+            print(len(v))
+            for ci in range(len(v)):  #loop over elements in collection
+                co={'_index':ind, '_type':c, '_parent': eid}
+                co.update(v[ci])
+                if 'pos' in co:
+                    hits=[]
+                    for xyz in co['pos']:
+                        hits.append({"x":xyz[0],"y":xyz[1],"z":xyz[2]})
+                    co['hits']=hits
+                    del co['pos']
+                print(co)
+                allDocs.append(co)
+
